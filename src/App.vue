48dbdc115ff8fa96c665351c0b7f48fe38b0650b
camychhetri <chhetricamy@gmail.com>
2019-11-26 16:21:14 -0500

    added dependencies and styled the form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Twitter Crawler</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/" title="Home tab">
+          Home
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
 
 <script>
+require('bootstrap')
 export default {
   name: 'App'
 }
@@ -18,6 +33,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container contact-form">
+    <h3 class="homeTitle">Data Collection Platform</h3>
+    <div class="row">
+      <div class="col-md-12">
+    <ul class="nav nav-tabs homeNav" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Twitter Keyword Based</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Twitter Location Based</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Twitter Users Based</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="rss-tab" data-toggle="tab" href="#rssfeed" role="tab" aria-controls="contact" aria-selected="false">RSS Feeds</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="stock-tab" data-toggle="tab" href="#stock" role="tab" aria-controls="stock" aria-selected="false">Search Stocks</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    <input type="text" id= "keywords" name="keywords" class="form-control" placeholder="Twitter Keywords seperated by commas" />
+  </div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+    <input type="text" id = "location" name="location" class="form-control" placeholder="Twitter Location seperated by commas" value="" />
+  </div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+    <input type="text" id = "users" name="users" class="form-control" placeholder="Twitter Users" value="" />
+ </div>
+ <div class="tab-pane fade" id="stock" role="tabpanel" aria-labelledby="contact-tab">
+    <input type="text" id = "stocks" name="stocks" class="form-control" placeholder="Twitter Stocks" value="" />
+ </div>
+ <div class="tab-pane fade" id="rssfeed" role="tabpanel" aria-labelledby="rss-tab">
+   <input type="text" id="rss_url" name="rss_url" class="form-control" placeholder="RSS Feeds URL" value="" />
+ </div>
+</div>
+<input type="button" class="btn btn-primary homeBtn" name="btnSubmit" value="Start Collection" onClick="ajaxRequest();"/>
+</div>
+</div>
+  </div>
+</template>
+<style scoped>
+.homeTitle{
+   margin-top: 25px;
+  margin-bottom: 25px;
+}
+.homeNav {
+margin-bottom: 15px;
+}
+.homeBtn{
+  margin-top: 15px;
+}
+</style>
